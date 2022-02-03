a1206b440d02c8ca8c24c794002dd059dbbe1a93
AntwanEmil <42111689+AntwanEmil@users.noreply.github.com>
2022-02-04 01:48:19 +0200

    open tcpdump in muted mode
    file created and is read successfully

--- IPS/udp/udp_prev.py
+++ IPS/udp/udp_prev.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-
 import subprocess
-print "start of scan ..."
-while 1:
-    subprocess.call("tcpdumbudp.sh")
-    #open the output file
-    #parse
-    #inner for loop on the file contents (with a flag)
-    ## if found 10 consec bla bla break and set break flag =1
-    # break
-    
+import os
+import time
+stream = os.popen('tcpdump -c 10 -i eth0 udp > udp.txt 2> /dev/null')
+#p = subprocess.Popen(
+#        ['tcpdump', '-i', ifname, '-w', '-', filter],
+#        stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+time.sleep(5)
 
-print "end"
+#p.terminate()
+
+#print (output)
+
+print ("end")
+
+
+
+f = open("udp.txt", "r")
+print(f.read())
