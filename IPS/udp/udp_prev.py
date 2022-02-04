d917ee0b148d338c57614213b49475132253cd34
AntwanEmil <42111689+AntwanEmil@users.noreply.github.com>
2022-02-04 12:54:15 +0200

    udp scanner with all features

--- IPS/udp/udp_prev.py
+++ IPS/udp/udp_prev.py
@@ -2,19 +2,45 @@
 import subprocess
 import os
 import time
-stream = os.popen('tcpdump -c 10 -i eth0 udp > udp.txt 2> /dev/null')
-#p = subprocess.Popen(
-#        ['tcpdump', '-i', ifname, '-w', '-', filter],
-#        stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-time.sleep(5)
+import re
 
-#p.terminate()
-
-#print (output)
-
-print ("end")
+blocked_ips = [""];
+while 1:
+    stream = os.popen('tcpdump -c 10 -i eth0 udp >> udp.txt 2> /dev/null')
+    time.sleep(5)
 
 
 
-f = open("udp.txt", "r")
-print(f.read())
+    print ("\t\tscanning ....")
+
+
+    flag =0
+    old_ip = "0.0.0.0"
+    file = open("udp.txt", "r")
+    for line in file:
+            try:
+                    ip=re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line).group()
+                    if ip in blocked_ips:
+                        continue
+                    if old_ip == ip:
+                            flag += 1
+                    else:
+                            flag =0
+                    old_ip = ip
+                    if flag == 9:
+                            print('########udp attack detected from ip: {}'.format(ip));
+                            ## calling the bash script to blacklist this ip
+                            blck = 'iptables -A INPUT -s ' + ip + ' -j DROP'
+                            os.popen(blck)
+                            p = subprocess.Popen(["iptables", "-A", "INPUT", "-s", ip,"-j", "DROP"], stdout=subprocess.PIPE)
+                            blck= 'iptables -A INPUT -s ' +ip +'-p tcp --destination-port 80 -j DROP'
+                            os.popen(blck) 
+                            blck = 'iptables -A INPUT -s ' +ip +'-p tcp --destination-port 443 -j DROP'
+                            os.popen(blck)
+                            blck  = 'sudo ufw deny from ' + ip
+                            os.popen(blck) 
+                            print('########this ip is auto-blocked from further communication')
+                            blocked_ips.append(ip)
+                            break
+            except Exception as e:
+                    flag = 0
